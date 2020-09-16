f2d9984f2d2e1d100969bd14de4121d8cdb688df
Pascal <pascal.barille@valdille-aubigne.fr>
2020-09-16 15:27:58 +0200

    Create doc_urba_com.sql

---
+++ doc_urba_com.sql
@@ -0,0 +1,29 @@
+-- Table: plui.doc_urba_com
+
+-- DROP TABLE plui.doc_urba_com;
+
+CREATE TABLE plui.doc_urba_com
+(
+    insee character(5) COLLATE pg_catalog."default",
+    idurba character varying(30) COLLATE pg_catalog."default",
+    the_geom geometry(Geometry,2154)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE plui.doc_urba_com
+    OWNER to postgres;
+
+GRANT SELECT ON TABLE plui.doc_urba_com TO g_valdilleaubigne;
+
+GRANT ALL ON TABLE plui.doc_urba_com TO postgres;
+-- Index: doc_urba_com_the_geom_1553528316973
+
+-- DROP INDEX plui.doc_urba_com_the_geom_1553528316973;
+
+CREATE INDEX doc_urba_com_the_geom_1553528316973
+    ON plui.doc_urba_com USING gist
+    (the_geom)
+    TABLESPACE pg_default;
