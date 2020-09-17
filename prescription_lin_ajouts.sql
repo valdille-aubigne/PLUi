ab800685e47cc60256efae799990e85f8dc1a384
Pascal <pascal.barille@valdille-aubigne.fr>
2020-09-17 09:22:00 +0200

    Create prescription_lin_ajouts.sql

---
+++ prescription_lin_ajouts.sql
@@ -0,0 +1,46 @@
+-- Table: plui.prescription_lin_ajouts
+
+-- DROP TABLE plui.prescription_lin_ajouts;
+
+CREATE TABLE plui.prescription_lin_ajouts
+(
+    libelle character varying(254) COLLATE pg_catalog."default" NOT NULL,
+    txt character varying(10) COLLATE pg_catalog."default",
+    typepsc character(2) COLLATE pg_catalog."default" NOT NULL,
+    stypepsc character(2) COLLATE pg_catalog."default" NOT NULL,
+    nomfic character varying(80) COLLATE pg_catalog."default",
+    urlfic character varying(254) COLLATE pg_catalog."default",
+    datvalid date,
+    idkey_ajout integer NOT NULL DEFAULT nextval('plui.prescription_lin_idkey_seq'::regclass),
+    idkey_du integer NOT NULL,
+    the_geom geometry(MultiLineString,2154),
+    CONSTRAINT prescription_lin_ajouts_pkey PRIMARY KEY (idkey_ajout)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE plui.prescription_lin_ajouts
+    OWNER to postgres;
+
+GRANT ALL ON TABLE plui.prescription_lin_ajouts TO postgres;
+
+-- Index: prescription_lin_ajouts_geom_gist
+
+-- DROP INDEX plui.prescription_lin_ajouts_geom_gist;
+
+CREATE INDEX prescription_lin_ajouts_geom_gist
+    ON plui.prescription_lin_ajouts USING gist
+    (the_geom)
+    TABLESPACE pg_default;
+
+-- Trigger: prescription_lin_ajout
+
+-- DROP TRIGGER prescription_lin_ajout ON plui.prescription_lin_ajouts;
+
+CREATE TRIGGER prescription_lin_ajout
+    AFTER INSERT OR DELETE
+    ON plui.prescription_lin_ajouts
+    FOR EACH ROW
+    EXECUTE PROCEDURE plui.prescription_lin_ajout(\x);
