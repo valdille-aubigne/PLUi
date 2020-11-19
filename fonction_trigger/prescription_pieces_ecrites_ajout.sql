feb0aa11060b198950c7f6b07c01eefeb3c5b8c8
Pascal <pascal.barille@valdille-aubigne.fr>
2020-11-19 15:56:51 +0100

    Create prescription_pieces_ecrites_ajout.sql

---
+++ fonction_trigger/prescription_pieces_ecrites_ajout.sql
@@ -0,0 +1,35 @@
+-- FUNCTION: plui.prescription_pieces_ecrites_ajout()
+
+-- DROP FUNCTION plui.prescription_pieces_ecrites_ajout();
+
+CREATE FUNCTION plui.prescription_pieces_ecrites_ajout()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$
+BEGIN
+
+IF TG_OP='INSERT' THEN
+EXECUTE 
+'UPDATE plui.doc_urba
+SET prescription_pieces_ecrites = array_append(prescription_pieces_ecrites,'||NEW.idkey_ajout||')
+WHERE idkey = '||NEW.idkey_du
+USING NEW ;
+END IF;
+
+IF TG_OP ='DELETE' THEN
+EXECUTE 
+'UPDATE plui.doc_urba
+SET prescription_pieces_ecrites = array_remove(prescription_pieces_ecrites,'||OLD.idkey_ajout||')
+WHERE idkey = '||OLD.idkey_du
+USING OLD ;
+END IF;
+
+RETURN NEW;
+
+END;
+$BODY$;
+
+ALTER FUNCTION plui.prescription_pieces_ecrites_ajout()
+    OWNER TO postgres;
