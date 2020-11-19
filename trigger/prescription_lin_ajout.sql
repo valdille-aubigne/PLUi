65d4cc7d8b01da4035081692e30e3ddba7e9e1d0
Pascal <pascal.barille@valdille-aubigne.fr>
2020-11-19 16:20:29 +0100

    Create prescription_lin_ajout.sql

---
+++ trigger/prescription_lin_ajout.sql
@@ -0,0 +1,9 @@
+-- Trigger: prescription_lin_ajout
+
+-- DROP TRIGGER prescription_lin_ajout ON plui.prescription_lin_ajouts;
+
+CREATE TRIGGER prescription_lin_ajout
+    AFTER INSERT OR DELETE
+    ON plui.prescription_lin_ajouts
+    FOR EACH ROW
+    EXECUTE PROCEDURE plui.prescription_lin_ajout();
