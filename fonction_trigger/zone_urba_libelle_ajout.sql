97aa884f2c3c0e1163dcf83d7ab4eb5d56c719ac
Pascal <pascal.barille@valdille-aubigne.fr>
2020-11-19 16:05:43 +0100

    Create zone_urba_libelle_ajout.sql

---
+++ fonction_trigger/zone_urba_libelle_ajout.sql
@@ -0,0 +1,35 @@
+-- FUNCTION: plui.zone_urba_libelle_ajout()
+
+-- DROP FUNCTION plui.zone_urba_libelle_ajout();
+
+CREATE FUNCTION plui.zone_urba_libelle_ajout()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$
+BEGIN
+
+IF TG_OP='INSERT' THEN
+EXECUTE 
+'UPDATE plui.doc_urba
+SET zone_urba_libelle = array_append(zone_urba_libelle,'||NEW.idkey_ajout||')
+WHERE idkey = '||NEW.idkey_du
+USING NEW ;
+END IF;
+
+IF TG_OP ='DELETE' THEN
+EXECUTE 
+'UPDATE plui.doc_urba
+SET zone_urba_libelle = array_remove(zone_urba_libelle,'||OLD.idkey_ajout||')
+WHERE idkey = '||OLD.idkey_du
+USING OLD ;
+END IF;
+
+RETURN NEW;
+
+END;
+$BODY$;
+
+ALTER FUNCTION plui.zone_urba_libelle_ajout()
+    OWNER TO postgres;
