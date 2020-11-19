ef640f3d66dc37174f6939d14ba97823c3c644ea
Pascal <pascal.barille@valdille-aubigne.fr>
2020-11-19 16:12:10 +0100

    Create info_lin_suppr.sql

---
+++ trigger/info_lin_suppr.sql
@@ -0,0 +1,9 @@
+-- Trigger: info_lin_suppr
+
+-- DROP TRIGGER info_lin_suppr ON plui.info_lin_suppr;
+
+CREATE TRIGGER info_lin_suppr
+    AFTER INSERT OR DELETE
+    ON plui.info_lin_suppr
+    FOR EACH ROW
+    EXECUTE PROCEDURE plui.info_lin_suppr();
