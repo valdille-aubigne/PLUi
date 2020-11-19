b7ffa0625f24d8314513bb7e60b0a02b0d42d20c
Pascal <pascal.barille@valdille-aubigne.fr>
2020-11-19 16:26:41 +0100

    Create sup_surf_suppr.sql

---
+++ trigger/sup_surf_suppr.sql
@@ -0,0 +1,9 @@
+-- Trigger: sup_surf_suppr
+
+-- DROP TRIGGER sup_surf_suppr ON plui.sup_surf_suppr;
+
+CREATE TRIGGER sup_surf_suppr
+    AFTER INSERT OR DELETE
+    ON plui.sup_surf_suppr
+    FOR EACH ROW
+    EXECUTE PROCEDURE plui.sup_surf_suppr();
