aad81574af9fceddbf1299da117aa0e1d2aae802
Pascal <pascal.barille@valdille-aubigne.fr>
2020-11-19 16:03:08 +0100

    Create sup_surf_ajout.sql

---
+++ fonction_trigger/sup_surf_ajout.sql
@@ -0,0 +1,35 @@
+-- FUNCTION: plui.sup_surf_ajout()
+
+-- DROP FUNCTION plui.sup_surf_ajout();
+
+CREATE FUNCTION plui.sup_surf_ajout()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$
+BEGIN
+
+IF TG_OP='INSERT' THEN
+EXECUTE 
+'UPDATE plui.doc_urba
+SET sup_surf = array_append(sup_surf,'||NEW.idkey_ajout||')
+WHERE idkey = '||NEW.idkey_du
+USING NEW ;
+END IF;
+
+IF TG_OP ='DELETE' THEN
+EXECUTE 
+'UPDATE plui.doc_urba
+SET sup_surf = array_remove(sup_surf,'||OLD.idkey_ajout||')
+WHERE idkey = '||OLD.idkey_du
+USING OLD ;
+END IF;
+
+RETURN NEW;
+
+END;
+$BODY$;
+
+ALTER FUNCTION plui.sup_surf_ajout()
+    OWNER TO postgres;
