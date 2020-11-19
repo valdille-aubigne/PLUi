95fe3d006feb8d4593d3eca47736b09eadf76a6e
Pascal <pascal.barille@valdille-aubigne.fr>
2020-11-19 16:00:44 +0100

    Create sup_lin_suppr.sql

---
+++ fonction_trigger/sup_lin_suppr.sql
@@ -0,0 +1,35 @@
+-- FUNCTION: plui.sup_lin_suppr()
+
+-- DROP FUNCTION plui.sup_lin_suppr();
+
+CREATE FUNCTION plui.sup_lin_suppr()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$
+BEGIN
+
+IF TG_OP='INSERT' THEN
+EXECUTE 
+'UPDATE plui.doc_urba
+SET sup_lin = array_remove(sup_lin,'||NEW.idkey||')
+WHERE idkey = '||NEW.idkey_du
+USING NEW ;
+END IF;
+
+IF TG_OP ='DELETE' THEN
+EXECUTE 
+'UPDATE plui.doc_urba
+SET sup_lin = array_append(sup_lin,'||OLD.idkey||')
+WHERE idkey = '||OLD.idkey_du
+USING OLD ;
+END IF;
+
+RETURN NEW;
+
+END;
+$BODY$;
+
+ALTER FUNCTION plui.sup_lin_suppr()
+    OWNER TO postgres;
