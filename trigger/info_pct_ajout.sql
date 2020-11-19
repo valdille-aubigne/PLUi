4dbf148521a520ce8cd418ed00fe1c04379cf68d
Pascal <pascal.barille@valdille-aubigne.fr>
2020-11-19 16:18:47 +0100

    Create info_pct_ajout.sql

---
+++ trigger/info_pct_ajout.sql
@@ -0,0 +1,9 @@
+-- Trigger: info_pct_ajout
+
+-- DROP TRIGGER info_pct_ajout ON plui.info_pct_ajouts;
+
+CREATE TRIGGER info_pct_ajout
+    AFTER INSERT OR DELETE
+    ON plui.info_pct_ajouts
+    FOR EACH ROW
+    EXECUTE PROCEDURE plui.info_pct_ajout();
