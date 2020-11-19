3dde00ce276eeaf91fd98fd5318ef86266b26ca7
Pascal <pascal.barille@valdille-aubigne.fr>
2020-11-19 16:04:52 +0100

    Create zone_urba_ajout.sql

---
+++ fonction_trigger/zone_urba_ajout.sql
@@ -0,0 +1,35 @@
+-- FUNCTION: plui.zone_urba_ajout()
+
+-- DROP FUNCTION plui.zone_urba_ajout();
+
+CREATE FUNCTION plui.zone_urba_ajout()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$
+BEGIN
+
+IF TG_OP='INSERT' THEN
+EXECUTE 
+'UPDATE plui.doc_urba
+SET zone_urba = array_append(zone_urba,'||NEW.idkey_ajout||')
+WHERE idkey = '||NEW.idkey_du
+USING NEW ;
+END IF;
+
+IF TG_OP ='DELETE' THEN
+EXECUTE 
+'UPDATE plui.doc_urba
+SET zone_urba = array_remove(zone_urba,'||OLD.idkey_ajout||')
+WHERE idkey = '||OLD.idkey_du
+USING OLD ;
+END IF;
+
+RETURN NEW;
+
+END;
+$BODY$;
+
+ALTER FUNCTION plui.zone_urba_ajout()
+    OWNER TO postgres;
