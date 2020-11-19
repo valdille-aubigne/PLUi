2cd7993face4fbc075fee273d6c6e2eb25cdf1ef
Pascal <pascal.barille@valdille-aubigne.fr>
2020-11-19 16:11:49 +0100

    Create info_lin_ajout.sql

---
+++ trigger/info_lin_ajout.sql
@@ -0,0 +1,9 @@
+-- Trigger: info_lin_ajout
+
+-- DROP TRIGGER info_lin_ajout ON plui.info_lin_ajouts;
+
+CREATE TRIGGER info_lin_ajout
+    AFTER INSERT OR DELETE
+    ON plui.info_lin_ajouts
+    FOR EACH ROW
+    EXECUTE PROCEDURE plui.info_lin_ajout();
