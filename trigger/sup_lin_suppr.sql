fcfe7e089f865840c8cdf4e3f6b60694e2fb4534
Pascal <pascal.barille@valdille-aubigne.fr>
2020-11-19 16:24:54 +0100

    Create sup_lin_suppr.sql

---
+++ trigger/sup_lin_suppr.sql
@@ -0,0 +1,9 @@
+-- Trigger: sup_lin_suppr
+
+-- DROP TRIGGER sup_lin_suppr ON plui.sup_lin_suppr;
+
+CREATE TRIGGER sup_lin_suppr
+    AFTER INSERT OR DELETE
+    ON plui.sup_lin_suppr
+    FOR EACH ROW
+    EXECUTE PROCEDURE plui.sup_lin_suppr();
