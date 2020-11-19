07891ae0b5e2c29321633e40e559b85e10440c26
Pascal <pascal.barille@valdille-aubigne.fr>
2020-11-19 16:25:44 +0100

    Create sup_pct_suppr.sql

---
+++ trigger/sup_pct_suppr.sql
@@ -0,0 +1,9 @@
+-- Trigger: sup_pct_suppr
+
+-- DROP TRIGGER sup_pct_suppr ON plui.sup_pct_suppr;
+
+CREATE TRIGGER sup_pct_suppr
+    AFTER INSERT OR DELETE
+    ON plui.sup_pct_suppr
+    FOR EACH ROW
+    EXECUTE PROCEDURE plui.sup_pct_suppr();
