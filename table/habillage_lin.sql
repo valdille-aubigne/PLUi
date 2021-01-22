ab49baffb66a5d46770b3d3a87213064c9525721
Pascal <pascal.barille@valdille-aubigne.fr>
2021-01-22 11:48:58 +0100

    Update habillage_lin.sql

--- table/habillage_lin.sql
+++ table/habillage_lin.sql
@@ -1,7 +1,6 @@
 CREATE TABLE plui.habillage_lin
 (
     nattrac character(40) COLLATE pg_catalog."default" NOT NULL,
-    idurba character varying(30) COLLATE pg_catalog."default" NOT NULL,
     idkey integer NOT NULL DEFAULT nextval('plui.habillage_lin_idkey_seq'::regclass),
     the_geom geometry(MultiLineString,2154),
     couleur character varying(11) COLLATE pg_catalog."default",
