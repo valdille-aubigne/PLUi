b4f121e2d5391562a1b7e43fdcfa733217394c10
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 11:07:12 +0100

    Update info_lin_ajout.sql

--- fonction_trigger/info_lin_ajout.sql
+++ fonction_trigger/info_lin_ajout.sql
@@ -26,6 +26,19 @@ WHERE idkey = '||OLD.idkey_du
 USING OLD ;
 END IF;
 
+IF TG_OP = 'UPDATE' AND NEW.idkey_du <> OLD.idkey_du THEN
+EXECUTE 
+'UPDATE plui.doc_urba
+SET info_lin = array_append(info_lin,'||NEW.idkey_ajout||')
+WHERE idkey = '||NEW.idkey_du
+USING NEW ;
+EXECUTE 
+'UPDATE plui.doc_urba
+SET info_lin = array_remove(info_lin,'||OLD.idkey_ajout||')
+WHERE idkey = '||OLD.idkey_du
+USING OLD ;
+END IF;
+
 RETURN NEW;
 
 END;
