b2e6859a4bc523c098eccf4ec1843b75c70e4ad5
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 16:13:09 +0100

    Update sup_pct_suppr.sql

--- trigger/sup_pct_suppr.sql
+++ trigger/sup_pct_suppr.sql
@@ -3,7 +3,7 @@
 -- DROP TRIGGER sup_pct_suppr ON plui.sup_pct_suppr;
 
 CREATE TRIGGER sup_pct_suppr
-    AFTER INSERT OR DELETE
+    AFTER INSERT OR DELETE OR UPDATE
     ON plui.sup_pct_suppr
     FOR EACH ROW
     EXECUTE PROCEDURE plui.sup_pct_suppr();
