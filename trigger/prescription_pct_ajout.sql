5022036a26d230e16f4b98a14f72778331703b8a
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 16:10:09 +0100

    Update prescription_pct_ajout.sql

--- trigger/prescription_pct_ajout.sql
+++ trigger/prescription_pct_ajout.sql
@@ -3,7 +3,7 @@
 -- DROP TRIGGER prescription_pct_ajout ON plui.prescription_pct_ajouts;
 
 CREATE TRIGGER prescription_pct_ajout
-    AFTER INSERT OR DELETE
+    AFTER INSERT OR DELETE OR UPDATE
     ON plui.prescription_pct_ajouts
     FOR EACH ROW
     EXECUTE PROCEDURE plui.prescription_pct_ajout();
