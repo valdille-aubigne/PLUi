1d6cfe11a24ded1056293bb8d5a73e1de9353697
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 16:11:07 +0100

    Delete prescription_surf.sql

--- trigger/prescription_surf.sql
+++
@@ -1,9 +0,0 @@
--- Trigger: prescription_surf
-
--- DROP TRIGGER prescription_surf ON plui.prescription_surf;
-
-CREATE TRIGGER prescription_surf
-    AFTER INSERT OR DELETE
-    ON plui.prescription_surf
-    FOR EACH ROW
-    EXECUTE PROCEDURE plui.prescription_surf();
