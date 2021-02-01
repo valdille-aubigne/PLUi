d2888ebfcfc07395cc3dc1f144c39eb8da5e05f2
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 16:10:24 +0100

    Update prescription_pct_suppr.sql

--- trigger/prescription_pct_suppr.sql
+++ trigger/prescription_pct_suppr.sql
@@ -3,7 +3,7 @@
 -- DROP TRIGGER prescription_pct_suppr ON plui.prescription_pct_suppr;
 
 CREATE TRIGGER prescription_pct_suppr
-    AFTER INSERT OR DELETE
+    AFTER INSERT OR DELETE OR UPDATE
     ON plui.prescription_pct_suppr
     FOR EACH ROW
     EXECUTE PROCEDURE plui.prescription_pct_suppr();
