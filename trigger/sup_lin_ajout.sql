0c74c9c070b5e8f41b1c8dc21e261345abaf0f78
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 16:12:12 +0100

    Update sup_lin_ajout.sql

--- trigger/sup_lin_ajout.sql
+++ trigger/sup_lin_ajout.sql
@@ -3,7 +3,7 @@
 -- DROP TRIGGER sup_lin_ajout ON plui.sup_lin_ajouts;
 
 CREATE TRIGGER sup_lin_ajout
-    AFTER INSERT OR DELETE
+    AFTER INSERT OR DELETE OR UPDATE
     ON plui.sup_lin_ajouts
     FOR EACH ROW
     EXECUTE PROCEDURE plui.sup_lin_ajout();
