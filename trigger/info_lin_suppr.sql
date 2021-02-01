ef0e044f46e9fb0d597d9edd5d3a43afb36fa016
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 16:07:42 +0100

    Update info_lin_suppr.sql

--- trigger/info_lin_suppr.sql
+++ trigger/info_lin_suppr.sql
@@ -3,7 +3,7 @@
 -- DROP TRIGGER info_lin_suppr ON plui.info_lin_suppr;
 
 CREATE TRIGGER info_lin_suppr
-    AFTER INSERT OR DELETE
+    AFTER INSERT OR DELETE OR UPDATE
     ON plui.info_lin_suppr
     FOR EACH ROW
     EXECUTE PROCEDURE plui.info_lin_suppr();
