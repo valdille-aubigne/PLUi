4bf43f4a37fdb47e069f5ad2c76d7be2b55847fd
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 11:29:01 +0100

    Update zone_urba_ajout.sql

--- fonction_trigger/zone_urba_ajout.sql
+++ fonction_trigger/zone_urba_ajout.sql
@@ -26,6 +26,19 @@ WHERE idkey = '||OLD.idkey_du
 USING OLD ;
 END IF;
 
+IF TG_OP = 'UPDATE' AND NEW.idkey_du <> OLD.idkey_du THEN
+EXECUTE 
+'UPDATE plui.doc_urba
+SET zone_urba = array_append(zone_urba,'||NEW.idkey_ajout||')
+WHERE idkey = '||NEW.idkey_du
+USING NEW ;
+EXECUTE 
+'UPDATE plui.doc_urba
+SET zone_urba = array_remove(zone_urba,'||OLD.idkey_ajout||')
+WHERE idkey = '||OLD.idkey_du
+USING OLD ;
+END IF;
+
 RETURN NEW;
 
 END;
