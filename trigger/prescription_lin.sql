bf23fc26710320d9244e8de6fa64c71265f2c3fe
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 16:09:07 +0100

    Delete prescription_lin.sql

--- trigger/prescription_lin.sql
+++
@@ -1,9 +0,0 @@
--- Trigger: prescription_lin
-
--- DROP TRIGGER prescription_lin ON plui.prescription_lin;
-
-CREATE TRIGGER prescription_lin
-    AFTER INSERT OR DELETE
-    ON plui.prescription_lin
-    FOR EACH ROW
-    EXECUTE PROCEDURE plui.prescription_lin();
