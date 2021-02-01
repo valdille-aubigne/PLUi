61628466ed29588a0d59c94a1d084a21fa0b5ab5
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 11:13:07 +0100

    Update prescription_lin_suppr.sql

--- fonction_trigger/prescription_lin_suppr.sql
+++ fonction_trigger/prescription_lin_suppr.sql
@@ -1,8 +1,8 @@
--- FUNCTION: plui.prescription_lin_ajout()
+-- FUNCTION: plui.prescription_lin_suppr()
 
--- DROP FUNCTION plui.prescription_lin_ajout();
+-- DROP FUNCTION plui.prescription_lin_suppr();
 
-CREATE FUNCTION plui.prescription_lin_ajout()
+CREATE FUNCTION plui.prescription_lin_suppr()
     RETURNS trigger
     LANGUAGE 'plpgsql'
     COST 100
@@ -13,7 +13,7 @@ BEGIN
 IF TG_OP='INSERT' THEN
 EXECUTE 
 'UPDATE plui.doc_urba
-SET prescription_lin = array_append(prescription_lin,'||NEW.idkey_ajout||')
+SET prescription_lin = array_remove(prescription_lin,'||NEW.idkey||')
 WHERE idkey = '||NEW.idkey_du
 USING NEW ;
 END IF;
@@ -21,7 +21,20 @@ END IF;
 IF TG_OP ='DELETE' THEN
 EXECUTE 
 'UPDATE plui.doc_urba
-SET prescription_lin = array_remove(prescription_lin,'||OLD.idkey_ajout||')
+SET prescription_lin = array_append(prescription_lin,'||OLD.idkey||')
+WHERE idkey = '||OLD.idkey_du
+USING OLD ;
+END IF;
+
+IF TG_OP = 'UPDATE' AND NEW.idkey_du <> OLD.idkey_du THEN
+EXECUTE 
+'UPDATE plui.doc_urba
+SET prescription_lin = array_remove(prescription_lin,'||NEW.idkey||')
+WHERE idkey = '||NEW.idkey_du
+USING NEW ;
+EXECUTE 
+'UPDATE plui.doc_urba
+SET prescription_lin = array_append(prescription_lin,'||OLD.idkey||')
 WHERE idkey = '||OLD.idkey_du
 USING OLD ;
 END IF;
@@ -31,5 +44,5 @@ RETURN NEW;
 END;
 $BODY$;
 
-ALTER FUNCTION plui.prescription_lin_ajout()
+ALTER FUNCTION plui.prescription_lin_suppr()
     OWNER TO postgres;
