fd0ed50d80671a2eea5fe8b96e75f854584d0027
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 16:10:41 +0100

    Update prescription_pieces_ecrites_ajout.sql

--- trigger/prescription_pieces_ecrites_ajout.sql
+++ trigger/prescription_pieces_ecrites_ajout.sql
@@ -3,7 +3,7 @@
 -- DROP TRIGGER prescription_pieces_ecrites_ajout ON plui.prescription_pieces_ecrites_ajouts;
 
 CREATE TRIGGER prescription_pieces_ecrites_ajout
-    AFTER INSERT OR DELETE
+    AFTER INSERT OR DELETE OR UPDATE
     ON plui.prescription_pieces_ecrites_ajouts
     FOR EACH ROW
     EXECUTE PROCEDURE plui.prescription_pieces_ecrites_ajout();
