a44d120445e61540491b38368043dfa72cadcdac
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 16:07:52 +0100

    Delete info_pct.sql

--- trigger/info_pct.sql
+++
@@ -1,9 +0,0 @@
--- Trigger: info_pct
-
--- DROP TRIGGER info_pct ON plui.info_pct;
-
-CREATE TRIGGER info_pct
-    AFTER INSERT OR DELETE
-    ON plui.info_pct
-    FOR EACH ROW
-    EXECUTE PROCEDURE plui.info_pct();
