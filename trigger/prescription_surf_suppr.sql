c7129e5022c0c8bf57f3f5c6de70cbf778ca3fb5
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 16:11:46 +0100

    Update prescription_surf_suppr.sql

--- trigger/prescription_surf_suppr.sql
+++ trigger/prescription_surf_suppr.sql
@@ -3,7 +3,7 @@
 -- DROP TRIGGER prescription_surf_suppr ON plui.prescription_surf_suppr;
 
 CREATE TRIGGER prescription_surf_suppr
-    AFTER INSERT OR DELETE
+    AFTER INSERT OR DELETE OR UPDATE
     ON plui.prescription_surf_suppr
     FOR EACH ROW
     EXECUTE PROCEDURE plui.prescription_surf_suppr();
