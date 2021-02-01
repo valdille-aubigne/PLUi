ee33bbac4955e5a0c959e6d7bbf65f7c7a72e4ee
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 16:13:40 +0100

    Update sup_surf_ajout.sql

--- trigger/sup_surf_ajout.sql
+++ trigger/sup_surf_ajout.sql
@@ -3,7 +3,7 @@
 -- DROP TRIGGER sup_surf_ajout ON plui.sup_surf_ajouts;
 
 CREATE TRIGGER sup_surf_ajout
-    AFTER INSERT OR DELETE
+    AFTER INSERT OR DELETE OR UPDATE
     ON plui.sup_surf_ajouts
     FOR EACH ROW
     EXECUTE PROCEDURE plui.sup_surf_ajout();
