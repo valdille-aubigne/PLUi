159dec90d71137c5c272ce6c5ce55ef13327e7b8
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 11:27:49 +0100

    Update sup_pct_suppr.sql

--- fonction_trigger/sup_pct_suppr.sql
+++ fonction_trigger/sup_pct_suppr.sql
@@ -26,6 +26,19 @@ WHERE idkey = '||OLD.idkey_du
 USING OLD ;
 END IF;
 
+IF TG_OP = 'UPDATE' AND NEW.idkey_du <> OLD.idkey_du THEN
+EXECUTE 
+'UPDATE plui.doc_urba
+SET sup_pct = array_remove(sup_pct,'||NEW.idkey||')
+WHERE idkey = '||NEW.idkey_du
+USING NEW ;
+EXECUTE 
+'UPDATE plui.doc_urba
+SET sup_pct = array_append(sup_pct,'||OLD.idkey||')
+WHERE idkey = '||OLD.idkey_du
+USING OLD ;
+END IF;
+
 RETURN NEW;
 
 END;
