6d54f1bf3b384adc177c8238e217cdd469879c4d
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 16:13:20 +0100

    Delete sup_surf.sql

--- trigger/sup_surf.sql
+++
@@ -1,9 +0,0 @@
--- Trigger: sup_surf
-
--- DROP TRIGGER sup_surf ON plui.sup_surf;
-
-CREATE TRIGGER sup_surf
-    AFTER INSERT OR DELETE
-    ON plui.sup_surf
-    FOR EACH ROW
-    EXECUTE PROCEDURE plui.sup_surf();
