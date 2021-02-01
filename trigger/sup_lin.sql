369b5c2fc364c59ac6a7d55203613c31319ac79d
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 16:11:58 +0100

    Delete sup_lin.sql

--- trigger/sup_lin.sql
+++
@@ -1,9 +0,0 @@
--- Trigger: sup_lin
-
--- DROP TRIGGER sup_lin ON plui.sup_lin;
-
-CREATE TRIGGER sup_lin
-    AFTER INSERT OR DELETE
-    ON plui.sup_lin
-    FOR EACH ROW
-    EXECUTE PROCEDURE plui.sup_lin();
