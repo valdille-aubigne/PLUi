90c78465f667795e0c368c715dcfcc6440153f27
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 16:12:28 +0100

    Update sup_lin_suppr.sql

--- trigger/sup_lin_suppr.sql
+++ trigger/sup_lin_suppr.sql
@@ -3,7 +3,7 @@
 -- DROP TRIGGER sup_lin_suppr ON plui.sup_lin_suppr;
 
 CREATE TRIGGER sup_lin_suppr
-    AFTER INSERT OR DELETE
+    AFTER INSERT OR DELETE OR UPDATE
     ON plui.sup_lin_suppr
     FOR EACH ROW
     EXECUTE PROCEDURE plui.sup_lin_suppr();
