6202f7de9dd096a0131a8c0de0e9b0fdff4db5c0
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 16:11:27 +0100

    Update prescription_surf_ajout.sql

--- trigger/prescription_surf_ajout.sql
+++ trigger/prescription_surf_ajout.sql
@@ -3,7 +3,7 @@
 -- DROP TRIGGER prescription_surf_ajout ON plui.prescription_surf_ajouts;
 
 CREATE TRIGGER prescription_surf_ajout
-    AFTER INSERT OR DELETE
+    AFTER INSERT OR DELETE OR UPDATE
     ON plui.prescription_surf_ajouts
     FOR EACH ROW
     EXECUTE PROCEDURE plui.prescription_surf_ajout();
