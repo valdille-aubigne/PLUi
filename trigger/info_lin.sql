aa45a0665769bcc0588b168ce86530afc99637ef
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 16:07:13 +0100

    Delete info_lin.sql

--- trigger/info_lin.sql
+++
@@ -1,9 +0,0 @@
--- Trigger: info_lin
-
--- DROP TRIGGER info_lin ON plui.info_lin;
-
-CREATE TRIGGER info_lin
-    AFTER INSERT OR DELETE
-    ON plui.info_lin
-    FOR EACH ROW
-    EXECUTE PROCEDURE plui.info_lin();
