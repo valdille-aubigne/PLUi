c4d4d777fbc5a729fb28c3b3b783e9c1bc32728e
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 16:08:28 +0100

    Delete info_surf.sql

--- trigger/info_surf.sql
+++
@@ -1,9 +0,0 @@
--- Trigger: info_surf
-
--- DROP TRIGGER info_surf ON plui.info_surf;
-
-CREATE TRIGGER info_surf
-    AFTER INSERT OR DELETE
-    ON plui.info_surf
-    FOR EACH ROW
-    EXECUTE PROCEDURE plui.info_surf();
