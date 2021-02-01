1b7f572c92b407c137d1b59a0fb71cd886ffe204
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 16:09:21 +0100

    Update prescription_lin_ajout.sql

--- trigger/prescription_lin_ajout.sql
+++ trigger/prescription_lin_ajout.sql
@@ -3,7 +3,7 @@
 -- DROP TRIGGER prescription_lin_ajout ON plui.prescription_lin_ajouts;
 
 CREATE TRIGGER prescription_lin_ajout
-    AFTER INSERT OR DELETE
+    AFTER INSERT OR DELETE OR UPDATE
     ON plui.prescription_lin_ajouts
     FOR EACH ROW
     EXECUTE PROCEDURE plui.prescription_lin_ajout();
