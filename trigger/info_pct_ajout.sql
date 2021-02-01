c7283586600138240af91b4a97b61934fc0c4d72
Pascal <pascal.barille@valdille-aubigne.fr>
2021-02-01 16:08:05 +0100

    Update info_pct_ajout.sql

--- trigger/info_pct_ajout.sql
+++ trigger/info_pct_ajout.sql
@@ -3,7 +3,7 @@
 -- DROP TRIGGER info_pct_ajout ON plui.info_pct_ajouts;
 
 CREATE TRIGGER info_pct_ajout
-    AFTER INSERT OR DELETE
+    AFTER INSERT OR DELETE OR UPDATE
     ON plui.info_pct_ajouts
     FOR EACH ROW
     EXECUTE PROCEDURE plui.info_pct_ajout();
